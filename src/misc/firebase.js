bd8c3fa257434f0a2e9874a24bcd181522359f9f
Varun Herlekar <varunherlekar22@gmail.com>
2021-02-02 15:57:18 +0530

    Initialized Firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebse from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCXy9gYzsI2RynmebtmEVoe-mfke32aU1w",
+    authDomain: "chat-web-app-59a38.firebaseapp.com",
+    databaseURL: "https://chat-web-app-59a38-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-59a38",
+    storageBucket: "chat-web-app-59a38.appspot.com",
+    messagingSenderId: "102079512712",
+    appId: "1:102079512712:web:e31060f8c5450e8b435f3d"
+  };
+
+  // eslint-disable-next-line no-undef
+ const app = firebase.initializeApp(config);
