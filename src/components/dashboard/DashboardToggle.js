2280d2534f45a0a02bc96d1269c4268fa01ccbf3
Varun Herlekar <varunherlekar22@gmail.com>
2021-02-13 11:01:54 +0530

    Add SignOut Button & display Username

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,14 +1,24 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable arrow-body-style */
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, {useCallback} from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
 import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
 
     const { isOpen, close, open  } = useModalState();
     const isMobile = useMediaQuery('(max-width: 992px)');
+
+    const onSignOut = useCallback(
+        () => {
+            auth.signOut();
+
+            Alert.info('Signed Out', 4000);
+
+            close();
+        },[close]);
 
 
     return (
@@ -17,7 +27,7 @@ const DashboardToggle = () => {
               <Icon icon="dashboard" /> Dashboard      
            </Button>  
            <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-               <Dashboard/>
+               <Dashboard onSignOut={onSignOut}/>
                </Drawer>    
 
         </>
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,8 +1,33 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { Button, Drawer, Icon } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
-    return <div>hello</div>;
+const Dashboard = ({onSignOut}) => {
+
+    const {profile} = useProfile();
+     
+    return <>
+
+        <Drawer.Header>
+            <Drawer.Title>
+                Dashboard
+            </Drawer.Title>
+        </Drawer.Header>
+
+        <Drawer.Body>
+            <h3>
+                Hey, {profile.name}
+            </h3>
+        </Drawer.Body>
+
+        <Drawer.Footer>
+            <Button block color="red" onClick={onSignOut}>
+             SignOut <Icon icon="sign-out"/>
+            </Button>
+        </Drawer.Footer>
+
+    </>
 
 };
 
